feea845d249024ba8de2b90ad1d27227155f0824
Arnold Averink <Fireblade75@users.noreply.github.com>
2021-03-17 10:25:11 +0100

    gherkin zinnen

--- src/test/java/com/example/bankapp/BankAccountTest.java
+++ src/test/java/com/example/bankapp/BankAccountTest.java
@@ -17,23 +17,34 @@ class BankAccountTest {
 
     @Test
     void successfulWithdraw() {
+        // Given an existing bank account is made with a balance of "800"
         BankAccount bankAccount = new BankAccount(customer, iban, 800, 0.05);
+        // When a withdrawal is made of less then the current balance
         boolean status = bankAccount.withdraw(400);
+        // Then the status of the withdrawal should be successful
         assertTrue(status);
+        // And the balance of the account should be 400
         assertEquals(bankAccount.getBalance(), 400, 0.001);
     }
 
     @Test
     void failedWithdraw() {
+        // Given an existing bank account is made with a balance of "800"
         BankAccount bankAccount = new BankAccount(customer, iban, 800, 0.05);
+        // When a withdrawal is made of more then the current balance
         boolean status = bankAccount.withdraw(900);
+        // Then the status of the withdrawal should be failed
         assertFalse(status);
+        // And the balance of the account should be 800
         assertEquals(bankAccount.getBalance(), 800, 0.001);
     }
 
     @Test
     void illegalWithdraw() {
+        // Given an existing bank account is made with a balance of "800"
         BankAccount bankAccount = new BankAccount(customer, iban, 800, 0.05);
+        // When a withdrawal is made with a negative amount
+        // Then an error should be thrown
         assertThrows(BankTransferFailedException.class, () -> {
             bankAccount.withdraw(-300);
         });
@@ -41,14 +52,19 @@ class BankAccountTest {
 
     @Test
     void deposit() {
+        // Given an existing bank account is made with a balance of "800"
         BankAccount bankAccount = new BankAccount(customer, iban, 800, 0.05);
+        // When a deposit is made of less then the current balance
         bankAccount.deposit(400);
         assertEquals(bankAccount.getBalance(), 1200, 0.001);
     }
 
     @Test
     void illegalDeposit() {
+        // Given an existing bank account is made with a balance of "800"
         BankAccount bankAccount = new BankAccount(customer, iban, 800, 0.05);
+        // When a deposit is made with a negative amount
+        // Then an error should be thrown
         assertThrows(BankTransferFailedException.class, () -> {
             bankAccount.deposit(-300);
         });
@@ -56,14 +72,21 @@ class BankAccountTest {
 
     @Test
     void calculateInterest() {
+        // Given an existing bank account is made with a balance of "800"
         BankAccount bankAccount = new BankAccount(customer, iban, 800, 0.10);
-        assertEquals(bankAccount.calculateInterest(), 80, 0.001);
+        // When the interest for one year is calculated
+        double interest = bankAccount.calculateInterest();
+        // Then the interest should be 80
+        assertEquals(interest, 80, 0.001);
     }
 
     @Test
     void applyInterest() {
+        // Given an existing bank account is made with a balance of "800"
         BankAccount bankAccount = new BankAccount(customer, iban, 800, 0.10);
+        // When the interest for one year is applied
         bankAccount.applyInterest();
+        // Then the balance of the bank account should be 880
         assertEquals(bankAccount.getBalance(), 880, 0.001);
     }
 
